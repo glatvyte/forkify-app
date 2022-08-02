bb979a2d790eda284591e52e4318b7fe3d7ce53e
glatvyte <glatvyte@gmail.com>
2022-08-02 15:51:59 +0300

    edit gitignore

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'b1492802-a2f1-4076-9cdc-a07fe4429f98';
+export const KEY = process.env.API_KEY;
 export const MODAL_CLOSE_SEC = 2.5;
